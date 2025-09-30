49927a04a020042c9f8495a39bd8316416cab4fc
Pravat Behera <beherapravat301@gmail.com>
2025-09-30 17:19:16 +0530

    feat(routes): add folder routes for folder management operations

--- apps/backend/src/routes/v0/index.ts
+++ apps/backend/src/routes/v0/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userRoute from "./user-route";
 import SubscriptionPlanRoutes from "./sybscriptionPlan-route"
-
+import fileRoute from "./file-route";
+import folderRoute from "./folder-route";
 const v0Routes = Router()
+
 v0Routes.use(userRoute);
 v0Routes.use(SubscriptionPlanRoutes);
+v0Routes.use(fileRoute);
+v0Routes.use(folderRoute);
 
-export default v0Routes
-
-
+export default v0Routes;
