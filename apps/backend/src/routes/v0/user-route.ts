2bf65b0681b3033398cf45ad436eb624a8f77bb2
Pravat Behera <beherapravat301@gmail.com>
2026-01-18 21:41:30 +0530

    feat: Implement modular backend architecture , replacing old controllers and routes.

--- apps/backend/src/routes/v0/user-route.ts
+++
@@ -1,9 +0,0 @@
-import { createAdminUser, createNewUser } from "@/controllers/auth-controller";
-import { Router } from "express";
-
-const router = Router();
-
-router.post('/user/create', createNewUser);
-router.post('/user/create-admin', createAdminUser);
-
-export default router;
