ff97c2943ab6d3c60d0d85ea98c25804a59ddb8b
Pravat Behera <beherapravat301@gmail.com>
2026-01-25 02:09:47 +0530

    remove next js frontend set

--- apps/frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="bg-orange-800 p-5 rounded-3xl font-bold text-2xl">Welcome to Filesphere</h1>
-    </div>
-  );
-}
-
